e6a8cd62f874331c141bbc6b823e9628df005a49
HassanAbdelmoniem <foxlsn125.com>
2026-03-15 01:41:33 +0200

    i hope it the las edite in Ajex

--- Ajex/clint.js
+++ Ajex/clint.js
@@ -1,46 +1,41 @@
 var selects = document.querySelectorAll("select");
-var first = selects[0]; 
-var second = selects[1]; 
+var first = selects[0];
+var second = selects[1];
 
 var xhr = new XMLHttpRequest();
-var data; 
-xhr.open('GET', 'rockbands.json');
+var data;
+xhr.open("GET", "rockbands.json");
 xhr.send();
-xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-        if (xhr.status >= 200 && xhr.status < 300) {
-            data = JSON.parse(xhr.responseText);
-            
-            for (var key in data) {
-                var hold = document.createElement("option");
-                hold.value = key;
-                hold.textContent = key; 
-                first.appendChild(hold);
-            }
-        }
-    }
-}
+xhr.onreadystatechange = function () {
+  if (xhr.readyState == 4) {
+    if (xhr.status >= 200 && xhr.status < 300) {
+      data = JSON.parse(xhr.responseText);
 
-
-first.addEventListener("change", function() {
-    var selected = this.value;
-    
-    second.innerHTML = '';
-    
-    if (selected && data[selected]) {
-        data[selected].forEach(function(select) {
-            var option = document.createElement("option");
-            option.value = select.value;
-            option.textContent = select.name; 
-            second.appendChild(option);
-        });
+      for (var key in data) {
+        var hold = document.createElement("option");
+        hold.value = key;
+        hold.textContent = key;
+        first.appendChild(hold);
+      }
     }
+  }
+};
+
+first.addEventListener("change", function () {
+  var selected = this.value;
+
+  second.innerHTML = "";
+
+    data[selected].forEach(function (select) {
+      var option = document.createElement("option");
+      option.value = select.value;
+      option.textContent = select.name;
+      second.appendChild(option);
+    });
 });
 
-second.addEventListener("change", function() {
-    var value = this.value;
-    
-    if (value && value !== "Artist") {
-        window.open(value, '_blank'); 
-    }
+second.addEventListener("change", function () {
+  var value = this.value;
+
+  window.open(value, "_blank");
 });
